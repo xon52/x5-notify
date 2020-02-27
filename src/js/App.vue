b174996739c80da9466d25af10c85a0c60d240fe
xon52 <30713944+xon52@users.noreply.github.com>
2020-02-27 17:14:32 +1000

    Formatting

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div
-    :class="`x5-n-notices x5-n-${$root.options.position}`"
-    :style="`z-index:${$root.options.zIndex}`"
-  >
-    <x5-notice
-      :key="m.key"
-      :options="m.options"
-      v-for="m in notices"
-      @close="close(m.key)"
-    >{{m.text}}</x5-notice>
+  <div :class="`x5-n-notices x5-n-${$root.options.position}`" :style="`z-index:${$root.options.zIndex}`">
+    <x5-notice :key="m.key" :options="m.options" v-for="m in notices" @close="close(m.key)">{{ m.text }}</x5-notice>
   </div>
 </template>
 
 <script>
-import x5Notice from "./Notice.vue"
+import x5Notice from './Notice.vue'
 
 export default {
-  name: "x5-N-App",
+  name: 'x5-N-App',
   components: { x5Notice },
   computed: {
     notices() {
       return this.$root.notices
-    }
+    },
   },
   methods: {
     close(key) {
       this.$root.remove(key)
-    }
-  }
+    },
+  },
 }
 </script>
--- src/js/Notice.vue
+++ src/js/Notice.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    ref="notice"
-    :class="`x5-n-notice x5-n-${typeClass} ${visible?'x5-n-visible':''}`"
-    @click="onClick"
-  >
+  <div ref="notice" :class="`x5-n-notice x5-n-${typeClass} ${visible ? 'x5-n-visible' : ''}`" @click="onClick">
     <div v-html="options.text"></div>
     <span v-if="options.closeOnClick" class="x5-n-click-to-dismiss">(click to dismiss)</span>
   </div>
@@ -11,34 +7,34 @@
 
 <script>
 export default {
-  name: "x5-N-Notice",
-  props: ["options"],
+  name: 'x5-N-Notice',
+  props: ['options'],
   data: () => ({
     timeout: null,
-    visible: false
+    visible: false,
   }),
   computed: {
     typeClass() {
-      const o = ["success", "warning", "error", "special", "info"]
-      return o.includes(this.options.type) ? this.options.type : "default"
-    }
+      const o = ['success', 'warning', 'error', 'special', 'info']
+      return o.includes(this.options.type) ? this.options.type : 'default'
+    },
   },
   methods: {
     onClick() {
       if (this.options.onClick) this.options.onClick()
-      this.$emit("click")
+      this.$emit('click')
       if (this.options.closeOnClick) this.close()
     },
     close() {
       clearTimeout(this.timeout)
       if (this.options.onClose) this.options.onClose()
       this.visible = false
-      setTimeout(() => this.$emit("close"), 250)
-    }
+      setTimeout(() => this.$emit('close'), 250)
+    },
   },
   mounted() {
     setTimeout(() => (this.visible = true), 50)
     this.timeout = setTimeout(() => this.close(), this.options.wait * 1000)
-  }
+  },
 }
 </script>
