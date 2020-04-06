4370447b54a0ae9cae4619ab560e599b6b4952da
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-06 10:40:19 +1000

    Renamed example logo and included html content example

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <div class="center">
-      <img class="logo" src="./x5-n-logo.svg" width="300" />
+      <img class="logo" src="./logo.svg" width="300" />
       <h1 class="title">x5-Notify Example</h1>
       <div class="row">
         <textarea id="text-input" v-model="text" rows="2" cols="40"></textarea>
@@ -11,6 +11,11 @@
           <button :key="type" class="button" @click="notify(type)">{{ type }}</button>
         </template>
       </div>
+      <!-- HTML/Icons -->
+      <div class="line">------------------------</div>
+      <h2>HTML/Icons</h2>
+      <p>The content/text of a notice does not have to be text and can be HTML.</p>
+      <button class="button" @click="notifyIcon()">ICON example</button>
       <div class="line">------------------------</div>
       <!-- Extra -->
       <h2>Callbacks</h2>
@@ -37,6 +42,12 @@ export default {
       this.$notify({
         type,
         text: this.text ? `${this.text}` : type
+      })
+    },
+    notifyIcon() {
+      this.$notify({
+        type: 'special',
+        text: `<svg viewBox="0 0 128 128"><path d="M0 8.934l49.854.158 14.167 24.47 14.432-24.47L128 8.935l-63.834 110.14zm126.98.637l-24.36.02-38.476 66.053L25.691 9.592.942 9.572l63.211 107.89zm-25.149-.008l-22.745.168-15.053 24.647L49.216 9.73l-22.794-.168 37.731 64.476zm-75.834-.17l23.002.009m-23.002-.01l23.002.01" fill="none"/><path d="M25.997 9.393l23.002.009L64.035 34.36 79.018 9.404 102 9.398 64.15 75.053z" fill="#35495e"/><path d="M.91 9.569l25.067-.172 38.15 65.659L101.98 9.401l25.11.026-62.966 108.06z" fill="#41b883"/></svg> <strong>VueJS</strong> is the best ❤`
       })
     },
     loop() {
